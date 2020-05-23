afe1c7b1cfd7e99a439690da90bfab9d569d137a
lavanya94lava <singhlavanya94@gmail.com>
2020-05-23 18:36:56 +0530

    add context API after redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
+import { createStore,applyMiddleware } from 'redux';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './components/App';
+import rootReducer from './reducers';
+
+
+const logger = function ({dispatch, getState}){
+  return function(next){
+    return function(action){
+      console.log("Action type ", action.type);
+      next(action);
+    }
+  }
+}
+
+const thunk = ({dispatch, getState}) =>(next) => (action) =>{
+  if(typeof action === 'function'){
+    action(dispatch);
+    return;
+  }
+  next(action);
+}
+
+const store = createStore(rootReducer,applyMiddleware(logger, thunk));
+
+export const StoreContext = createContext();
+
+console.log("StoreContext", StoreContext);
+
+
+class Provider extends React.Component{
+  render(){
+    const {store } = this.props;
+    return (
+    <StoreContext.Provider value = {store}>
+      {this.props.children}
+    </StoreContext.Provider>
+    )
+  }
+}
+
+// console.log("before state", store.getState());
+
+// store.dispatch({
+//   type:'ADD_MOVIES',
+//   movies:[{name:'Superman'}]
+// });
+
+// console.log("after state", store.getState());
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store = {store}>
+    <App store = {store} />
+  </Provider>,
   document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
